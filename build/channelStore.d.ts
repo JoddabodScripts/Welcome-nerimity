e9137d168d883ad03b8e4dc37eee2d2951ee1aac
Joud j <joddabod5@gmail.com>
2026-06-17 13:13:21 +0400

    chore: remove compiled build output files

--- build/channelStore.d.ts
+++
@@ -1,5 +0,0 @@
-export declare function getWelcomeChannel(serverId: string): string | undefined;
-export declare function isChannelLocked(serverId: string): boolean;
-export declare function setWelcomeChannel(serverId: string, channelId: string, locked?: boolean): void;
-export declare function lockWelcomeChannel(serverId: string, channelId: string): void;
-//# sourceMappingURL=channelStore.d.ts.map
--- build/welcomeCard.d.ts
+++
@@ -1,7 +0,0 @@
-export interface WelcomeEmbedOptions {
-    avatarUrl: string;
-    username: string;
-    serverName: string;
-}
-export declare function buildWelcomeEmbed(opts: WelcomeEmbedOptions): string;
-//# sourceMappingURL=welcomeCard.d.ts.map
